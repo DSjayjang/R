9f920135cf90a473418aab4333348d9124268625
DSjayjang <a52071194@gmail.com>
2025-03-20 21:24:26 +0900

    3rd

--- R_basic/3rd_code.R
+++ R_basic/3rd_code.R
@@ -21,7 +21,7 @@ mat4
 ## 행렬의 이름 설정1
 rownames(mat4) <- c('r1', 'r2', 'r3')
 colnames(mat4) <- c('c1', 'c2')
-mat4
+
 
 ## 행렬의 이름 설정2
 rnames <- c('r1', 'r2')
@@ -41,11 +41,8 @@ mat[2, 3] # 2행 3열
 
 mat[1, ] # 1행 전체 출력
 mat[, 2] # 2열 전체 출력
-
-mat[, c(1, 3)] # 1열, 3열 전체 출력
-
 mat[, -2] # 2번째 열은 제외하고 출력
-
+mat[, c(1, 3)] # 1열, 3열 전체 출력
 
 
 # 리스트 (list)
@@ -82,6 +79,7 @@ x
 df <- data.frame(x = c(1, 2, 3, 4, 5),
                  y = c(2, 4, 6, 8, 10),
                  z = c('M', 'F', 'M', 'F', 'M'))
+df
 
 ## 데이터 프레임의 차원 확인
 dim(df) # 행, 열
@@ -100,13 +98,12 @@ df[1, ] # 1행
 df[, 1] # 1열
 
 
-
 # 작업 공간 설정
 ## 현재 작업 중인 경로 확인
 getwd()
 
 ## 작업할 경로 재설정
-setwd('C:\\Users\\linde\\OneDrive\\Desktop\\1. Lecture_DataScience\\1-2\\5. 기초통계실습1\\2. 강의자료\\3rd')
+setwd('경로')
 getwd()
 
 
@@ -118,23 +115,25 @@ list.files('./') # 현재 경로에 있는 파일 확인
 data <- read.csv(file = 'data.csv', header = T)
 data
 
-### 출력
+data2 <- read.csv(file = 'data.csv', header = F)
+data2
+
+
+### 출력(저장)
 write.csv(data, file = 'new_data.csv')
 write.csv(data, file = 'new_data.csv', row.names = F)
 
 
 ## 텍스트(txt) 파일 입출력
 ### 입력
-
-data2 <- read.table(file = 'data.txt', header = TRUE, sep = '\t')
-data2
-
-data3 <- read.table(file = 'data.txt', header = FALSE)
+data3 <- read.table(file = 'data.txt', header = TRUE)
 data3
 
 readLines("data.txt")
+data4 <- read.table(file = 'data.txt', header = TRUE, sep = '\t')
+data4
 
-### 출력
+### 출력(저장)
 write.table(data, file = 'new_data.txt', sep = ',')
 write.table(data, file = 'new_data2.txt', sep = ',', quote = FALSE)
 write.table(data, file = 'new_data3.txt', sep = ',', quote = FALSE, row.names = F)
