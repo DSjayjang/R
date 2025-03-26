d18cbfc30db0cb731d9e371b941790a567546db6
DSjayjang <a52071194@gmail.com>
2025-03-27 00:43:08 +0900

    Update 4th_code.R

--- R_basic/4th_code.R
+++ R_basic/4th_code.R
@@ -27,3 +27,73 @@ if (grade2 >= 80){
 
 
 ## 반복문 (for)
+for(i in 1:5){
+  print(i)
+}
+
+## 반복문과 조건문
+vec <- c(10, 22, 3, 15, 8, 5)
+
+for(i in vec){
+  if(i >= 10){
+    print('10 이상')
+  }
+  else{
+    print('10 미만')
+  }
+}
+
+for(i in vec){
+  if(i >= 10){
+    print(paste('i=', i, '는 10 이상'))
+  }
+  else{
+    print(paste('i=', i, '는 10 미만'))
+  }
+}
+
+
+## 반복문 (while)
+var <- 1
+while (var <= 10){
+  print(var)
+
+  var <- var + 1
+}
+
+
+i <- 1
+while (i <= 10){
+  if(i %% 2 != 0){
+    print(i) # 2로 나눈 나머지가 0이 아닌 경우. 즉, 홀수
+  }
+  i <- i + 1
+}
+
+## 구구단
+for(i in 1:9){
+  print(paste0(i, "단 시작"))
+  
+  for(j in 1:9){
+    print(paste(i, "*", j, "=", i*j))
+  }
+}
+
+
+# next
+i <- 0
+
+while(i <= 10){
+  i <- i + 1
+  if(i %% 2 != 0){
+    next
+  }
+  print(i)
+}
+
+
+
+
+
+
+
