5546f80a1f97788855cbcb5206aba417f461c635
DSjayjang <a52071194@gmail.com>
2025-05-09 11:42:37 +0900

    Update 10th_code.R

--- R_basic/10th_code.R
+++ R_basic/10th_code.R
@@ -1,9 +1,12 @@
 rm(list=ls())
 
 # 데이터 불러오기
-setwd('~~~')
+setwd('')
+
 df <- read.csv('df.csv', header = TRUE)
-df
+head(df)
+str(df)
+summary(df)
 
 df_joint <- read.csv('df_joint.csv', header = TRUE)
 df_joint
@@ -11,12 +14,13 @@ df_joint
 # 모집단 크기
 n = nrow(df); n
 
-# 결합 확률분포
-joint <- table(df); joint
+# 결합 확률분포 생성
+joint <- table(df) / n
+joint
 
 # 주변 확률분포
-X_margin <- margin.table(joint, margin = 1) / n; X_margin
-Y_margin <- margin.table(joint, margin = 2) / n; Y_margin
+X_margin <- margin.table(joint, margin = 1); X_margin
+Y_margin <- margin.table(joint, margin = 2); Y_margin
 
 ## 주변 확률분포의 기댓값
 mu_X <- sum(unique(df$X) * X_margin); mu_X # 모평균
@@ -30,9 +34,9 @@ dev_Y <- unique(df$Y) - mu_Y; dev_Y # 편차
 var_Y <- sum(dev_Y^2 * Y_margin); var_Y # 모분산
 
 #########################################################################
-# 데이터가 모집단의 표본이라고 가정한다면,
+# 데이터가 모집단에서 추출한 표본이라면?
 ## 표본분산
-var(df$X) 
+var(df$X)
 
 ## 표본분산 직접 계산하기
 svar_X <- sum((df$X - mean(df$X))^2) / (n-1); svar_X
@@ -40,12 +44,14 @@ svar_X <- sum((df$X - mean(df$X))^2) / (n-1); svar_X
 # 모분산 vs. 표본분산
 var_X; svar_X
 
-svar_X2 <- sum((df$X - mean(df$X))^2) / (n); svar_X2 # 모분산과 동일
+svar_X2 <- sum((df$X - mean(df$X))^2) / (n); svar_X2 # 모집단으로 가정했을 때는 모분산과 동일
 #########################################################################
 
 
 
 # 결합 확률분포의 기댓값
+df_joint
+
 mu_XY <- sum(df_joint$XY * df_joint$Prob); mu_XY
 
 # 공분산 Cov(X,Y) = E[XY] - E[X]E[Y]
@@ -57,7 +63,5 @@ sum((df$X - mean(df$X)) * (df$Y - mean(df$Y))) / (n-1)
 sum((df$X - mean(df$X)) * (df$Y - mean(df$Y))) / (n) # 모공분산과 동일
 
 
-# 상관계수 cor(X,Y) = cov(X,Y)/(var(X)*var(Y))^{1/2}
-cor_XY <- cov_XY / sqrt(var_X * var_Y); cor_XY # 모상관계수
-cor(df$X, df$Y) # 표본 상관계수
 
+# End
