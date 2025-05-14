8a0a9630e2267ef7cbf4a57aab1633e4baaf0f30
DSjayjang <a52071194@gmail.com>
2025-05-14 13:20:04 +0900

    Update 11th_code.R

--- R_basic/11th_code.R
+++ R_basic/11th_code.R
@@ -1,4 +1,40 @@
 # 베르누이 분포 (bernoulli distribution)
 
 # 이항 분포 (binomial distribution)
-rbinom(n = 1, size = 10, prob = 0.5)
+n_rv = 1000
+n_trials = 10
+p1 = 0.3
+p2 = 0.5
+p3 = 0.9
+
+set.seed(1234)
+X <- rbinom(n = n_rv, size = n_trials, prob = p1); X
+Y <- rbinom(n = n_rv, size = n_trials, prob = p2); Y
+Z <- rbinom(n = n_rv, size = n_trials, prob = p3); Z
+
+# 등장 횟수
+table(X)
+table(Y)
+table(Z)
+
+# 기댓값
+# E[X] = np
+mean(X)
+n_trials * p1
+
+mean(Y)
+10 * 0.5
+
+mean(Z)
+10 * 0.9
+
+# 분산
+# Var[X] = np(1-p)
+var(X)
+10 * 0.3 * 0.7
+
+var(Y)
+10 * 0.5 * 0.5
+
+var(Z)
+10 * 0.9 * 0.1
