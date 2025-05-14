2b3d08fdf9f4a53bcdc847242a25f333f120a31d
DSjayjang <a52071194@gmail.com>
2025-05-15 00:11:52 +0900

    Update 11th_code.R

--- R_basic/11th_code.R
+++ R_basic/11th_code.R
@@ -1,40 +1,106 @@
+rm(list=ls())
+set.seed(2025)
+
 # 베르누이 분포 (bernoulli distribution)
+sample(c('succ', 'fail'), 1, prob = c(0.6, 0.4))
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
 
 # 이항 분포 (binomial distribution)
-n_rv = 1000
+sample(c('succ', 'fail'), 10, replace = T, prob = c(0.6, 0.4))
+sum(sample(c(1, 0), 10, replace = T, prob = c(0.6, 0.4)))
+rbinom(n = 1, size = 10, prob = 0.6)
+
+# rbinom(n, size, prob)
+# 이항 분포 ~bin(size, prob)를 따르는 난수 n개 생성
+n_rn = 1000
 n_trials = 10
 p1 = 0.3
 p2 = 0.5
 p3 = 0.9
 
-set.seed(1234)
-X <- rbinom(n = n_rv, size = n_trials, prob = p1); X
-Y <- rbinom(n = n_rv, size = n_trials, prob = p2); Y
-Z <- rbinom(n = n_rv, size = n_trials, prob = p3); Z
+X <- rbinom(n = n_rn, size = n_trials, prob = p1); X
+Y <- rbinom(n = n_rn, size = n_trials, prob = p2); Y
+Z <- rbinom(n = n_rn, size = n_trials, prob = p3); Z
 
 # 등장 횟수
 table(X)
 table(Y)
 table(Z)
 
-# 기댓값
+# 이항분포의 기댓값
 # E[X] = np
-mean(X)
 n_trials * p1
+mean(X)
 
+n_trials * p2
 mean(Y)
-10 * 0.5
 
+n_trials * p3
 mean(Z)
-10 * 0.9
 
-# 분산
+# 이항분포의 분산
 # Var[X] = np(1-p)
+n_trials * p1 * (1 - p1)
 var(X)
-10 * 0.3 * 0.7
 
+n_trials * p2 * (1 - p2)
 var(Y)
-10 * 0.5 * 0.5
 
+n_trials * p3 * (1 - p3)
 var(Z)
-10 * 0.9 * 0.1
+
+
+# dbinom(x, size, prob)
+# 성공 확률이 prob이고, size 만큼 시행했을 때 x 번 성공할 확률
+dbinom(3, size = 5, prob = 0.5) # 동전을 5번 던져 앞면이 3번 나올 확률
+dbinom(0:5, size = 5, prob = 0.5) # 동전을 5번 던져 앞면이 0,1,2,3,4,5번 나올 확률
+
+############################################
+## 이산형 확률 변수의 기댓값
+## sum(X * P(X=x))
+mu <- sum(0:5 * dbinom(0:5, size = 5, prob = 0.5)); mu
+## 이항 분포의 기댓값: E[X] = np
+5 * 0.5
+
+## 이산형 확률 변수의 분산
+sum((0:5 - mu)^2 * dbinom(0:5, size = 5, prob = 0.5))
+## 이항 분포의 분산: Var[X] = np(1-p)
+5 * 0.5 * 0.5
+############################################
+
+# 확률질량함수
+barplot(dbinom(0:5, size = 5, prob = 0.5))
+barplot(dbinom(0:30, size = 30, prob = 0.5))
+barplot(dbinom(0:30, size = 30, prob = 0.3))
+barplot(dbinom(0:30, size = 30, prob = 0.7))
+
+
+# pbinom(q, size, prob, lower.tail = TRUE)
+# 이항분포의 누적확률 계산
+pbinom(3, size = 5, prob = 0.5) # P[X <= x]
+pbinom(3, size = 5, prob = 0.5, lower.tail = FALSE) # P[X>x]
+pbinom(0:5, size = 5, prob = 0.5)
+
+
+# qbinom(p, size, prob, lower.tail = TRUE)
+# 이항 분포의 분위수 계산
+p <- pbinom(3, size = 5, prob = 0.5); p
+qbinom(p, size = 5, prob = 0.5)
+
+p <- pbinom(0:5, size = 5, prob = 0.5); p
+qbinom(p, size = 5, prob = 0.5)
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+# 초기하 분포
+
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+# 포아송 분포
