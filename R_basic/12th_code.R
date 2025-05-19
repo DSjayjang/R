21bec720e02c538cd7975b73162637c8d6ed6e7e
DSjayjang <a52071194@gmail.com>
2025-05-19 10:02:48 +0900

    Create 12th_code.R

---
+++ R_basic/12th_code.R
@@ -0,0 +1,11 @@
+
+# 초기하 분포
+# X ~ hypergeometirc(N, n, D)
+
+# rhyper(nn, m, n, k)
+n_rn <- 1000
+white_ball <- 4
+black_ball <- 6
+n <- 3
+
+rhpyer(nn = n_rn, m = white_ball, n = black_ball, k = n)
